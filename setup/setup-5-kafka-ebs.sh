9bae4fd6213586edb211ea787e4519dcb0640bf6
Qiao, Joe <qiao.y@pg.com>
2022-05-20 17:04:23 +0800

    azure mount ssd to vm

--- setup/setup-5-kafka-ebs.sh
+++ setup/setup-5-kafka-ebs.sh
@@ -8,7 +8,7 @@ sudo su
 lsblk
 
 # we verify the disk is empty - should return "data"
-file -s /dev/xvdf
+file -s /dev/sdc
 
 # Note on Kafka: it's better to format volumes as xfs:
 # https://kafka.apache.org/documentation/#filesystems
@@ -16,23 +16,30 @@ file -s /dev/xvdf
 apt-get install -y xfsprogs
 
 # create a partition
-fdisk /dev/xvdf
+# fdisk /dev/sdc
+parted /dev/sdc --script mklabel gpt mkpart xfspart xfs 0% 100%
 
 # format as xfs
-mkfs.xfs -f /dev/xvdf
+mkfs.xfs -f /dev/sdc
 
 # create kafka directory
 mkdir /data/kafka
 # mount volume
-mount -t xfs /dev/xvdf /data/kafka
+mount -t xfs /dev/sdc /data/kafka
 # add permissions to kafka directory
-chown -R ubuntu:ubuntu /data/kafka
+chown -R root:root /data/kafka
 # check it's working
 df -h /data/kafka
 
 # EBS Automount On Reboot
 cp /etc/fstab /etc/fstab.bak # backup
-echo '/dev/xvdf /data/kafka xfs defaults 0 0' >> /etc/fstab
+blkid
+echo 'UUID=db6742e7-3ca0-4a5d-aa5b-0d3a1d03fe18 /data/kafka xfs   defaults,nofail   1   2' >> /etc/fstab
+cat /etc/fstab
+# echo '/dev/xvdf /data/kafka xfs defaults 0 0' >> /etc/fstab
+
+# Verify
+lsblk -o NAME,HCTL,SIZE,MOUNTPOINT | grep -i "sd"
 
 # reboot to test actions
 reboot
