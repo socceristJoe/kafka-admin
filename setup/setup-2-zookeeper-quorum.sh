1709410c5948ffdb436394a57540678b522643d3
Qiao, Joe <qiao.y@pg.com>
2022-05-20 12:16:33 +0800

    finish quorom

--- setup/setup-2-zookeeper-quorum.sh
+++ setup/setup-2-zookeeper-quorum.sh
@@ -9,8 +9,10 @@ echo "3" > /data/zookeeper/myid
 # edit the zookeeper settings
 rm /root/kafka/config/zookeeper.properties
 nano /root/kafka/config/zookeeper.properties
+cat /root/kafka/config/zookeeper.properties
 # restart the zookeeper service
 service zookeeper stop
+systemctl daemon-reload
 service zookeeper restart
 service zookeeper start
 service zookeeper status
@@ -20,10 +22,18 @@ nc -vz localhost 2181
 nc -vz zookeeper1 2181
 nc -vz zookeeper2 2181
 nc -vz zookeeper3 2181
-nc -vz localhost 2888
-nc -vz localhost 3888
+
+nc -vz zookeeper1 2888
+nc -vz zookeeper2 2888
+nc -vz zookeeper3 2888
+
+nc -vz zookeeper1 3888
+nc -vz zookeeper2 3888
+nc -vz zookeeper3 3888
+
 echo "ruok" | nc localhost 2181 ; echo
 echo "stat" | nc localhost 2181 ; echo
+
 bin/zookeeper-shell.sh localhost:2181
 # not happy
 ls /
